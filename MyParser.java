3c3d6a29335c49e778f26751a389b3fc5bc04d2c
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2021-12-02 23:10:18 -0800

    basic scanner file included
    need to figure out how to "parse" the input file

--- MyParser.java
+++ MyParser.java
@@ -1,8 +1,21 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class MyParser {
 
     public void parseText(File p) {
+        try (Scanner scan = new Scanner(p)) 
+        {
+            while(scan.hasNextLine())
+            {
+                
+            }
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("FILE NOT FOUND");
+            //e.printStackTrace();
+        }
         
     }
     
