29bce476c2fb0e7166bc07680a4cacbd45d7f47e
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2021-12-06 02:11:04 -0800

    need to load into valResult state table vals

--- Parser/MyParser.java
+++ Parser/MyParser.java
@@ -4,10 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class MyParser {
 
     public static String everything = "";
+    public static ArrayList<String> scanResult = new ArrayList<>();
+    public static ArrayList<String> valResult = new ArrayList<>();
+    public Stack<String> stk = new Stack<>();
+    int counter = 0;
 
     public void parseText(String file) {
         try {
@@ -32,8 +38,32 @@ public class MyParser {
             System.out.println("Input Error");
         }
         System.out.println(everything);
+        
+        //removing the unneeded words
+        //remove "Token Discovered is " & "->""
         everything = everything.replaceAll("Token Discovered is", "");
+        everything = everything.replaceAll(" ->", "");
         System.out.println("REPLACED");
         System.out.println(everything);
+        stk.push("1");
+        String tem[] = everything.strip().split(" ");
+        //fill arraylist with scanned results
+        for (String string : tem) {
+            string = string.trim();
+            if (!string.equals("")) {
+                scanResult.add(string);
+            }
+        }
+        removeFromList(scanResult, "comment");
+        System.out.print(scanResult);
+        
+    }
+
+    public void removeFromList(ArrayList<String> list, String word) {
+        ArrayList<String> del = new ArrayList<>();
+        del.add(word);
+        if (list.contains(word)) {
+            list.removeAll(del);
+        }
     }
 }
---
+++ Parser/ParserTokens.java
@@ -0,0 +1,10 @@
+package Parser;
+
+public class ParserTokens {
+    
+    String token_lookup[] = {"0","library","program","id",";","var",":",",","int","real","sci","currency",
+                        "string","begin","return",":=","device_open","file","for","to","do","repeat",
+                        "until","(",")","while","if","then","else","device_close","read_from_device",
+                        "write_to_device","writeln","readln","<",">","<=",">=","==","!=","+","-","*",
+                        "/","abs","fabs","stop","$"};
+}
